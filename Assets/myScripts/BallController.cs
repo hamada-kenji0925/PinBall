b953c7608777482d3e4328449291d9ae113c1a8b
hamada.kenji <hamada.kenji0925@gmail.com>
2017-06-03 09:47:16 +0900

    PinBallプロジェクト　初めてのコミット
    Lesson5で作成したPinBallゲームです。課題、発展課題に取り組んでいます。

---
+++ Assets/myScripts/BallController.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+//UI使う際に必要↓
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour {
+
+	//ボールが見える可能性のあるz軸の最大値
+	private float visiblePosZ = -6.5f;
+
+	//ゲームオーバーを表示するテキスト
+	private GameObject gameoverText;
+	//スコアを表示するテキスト
+	private GameObject scoreText;
+
+	//プレイヤー持ち点
+	private int myScore;
+
+	//SmallStar点数
+	private int s_star = 1;
+	//LargeStar点数
+	private int l_star = 5;
+	//SmallCloud点数
+	private int s_cloud = 10;
+	//LargeCloud点数
+	private int l_cloud = 25;
+
+	//衝突したタグ名格納の宣言
+	private string tagName = "none";
+
+	// Use this for initialization
+	void Start () {
+		//シーン中のgameoverTextオブジェクトを取得
+		this.gameoverText = GameObject.Find("GameOverText");
+		//シーン中のscoreTextオブジェクトを取得
+		this.scoreText = GameObject.Find("ScoreText");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//ボールが画面外に出た場合
+		if (this.transform.position.z < this.visiblePosZ) {
+			//GameOverTextにゲームオーバーを表示
+			this.gameoverText.GetComponent<Text>().text = "Game Over";
+		}
+
+		//衝突したオブジェクト毎に得点を加算
+		switch (this.tagName) {
+		case "SmallStarTag":
+			this.myScore += this.s_star;
+			break;
+		case "LargeStarTag":
+			this.myScore += this.l_star;
+			break;
+		case "SmallCloudTag":
+			this.myScore += s_cloud;
+			break;
+		case "LargeCloudTag":
+			this.myScore += l_cloud;
+			break;
+		default:
+			break;
+		}
+
+		//scoreTextの得点を更新
+		this.scoreText.GetComponent<Text>().text = this.myScore.ToString();
+		//tagNameの初期化
+		this.tagName = "none";
+	}
+
+	void OnCollisionEnter(Collision other){
+		//衝突したタグ名をtagName変数に格納
+		this.tagName = other.gameObject.tag;
+
+	}
+}
---
+++ Assets/myScripts/FripperController.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FripperController : MonoBehaviour {
+
+	//HingeJointコンポーネントを入れる
+	private HingeJoint myHingeJoint;
+
+	//初期の傾き
+	private float defaultAngel = 20;
+	//弾いた時の傾き
+	private float flickAngel = -20;
+
+	//画面幅の境界線座標の取得
+	private int winBorder = Screen.width / 2;
+
+	// Use this for initialization
+	void Start () {
+		//HingeJointコンポーネントを取得
+		this.myHingeJoint = GetComponent<HingeJoint>();
+
+		//フリッパーの傾きを設定
+		SetAngel(this.defaultAngel);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//矢印キー左を押した時左フリッパーを動かす
+		if (Input.GetKeyDown (KeyCode.LeftArrow) && tag == "LeftFripperTag") {
+			SetAngel (this.flickAngel);
+		}
+		//矢印キー右を押した時右フリッパーを動かす
+		if (Input.GetKeyDown (KeyCode.RightArrow) && tag == "RightFripperTag") {
+			SetAngel (this.flickAngel);
+		}
+
+		//矢印キー左を離した左フリッパーを元に戻す
+		if (Input.GetKeyUp (KeyCode.LeftArrow) && tag == "LeftFripperTag") {
+			SetAngel (this.defaultAngel);
+		}
+		//矢印キー右を離した時右フリッパーを元に戻す
+		if (Input.GetKeyUp (KeyCode.RightArrow) && tag == "RightFripperTag") {
+			SetAngel (this.defaultAngel);
+		}
+
+		//タップ判定処理
+		foreach (Touch touch in Input.touches) {
+			//タップされた位置と境界線との比較
+			if (touch.position.x < this.winBorder) {
+				//画面左側をタップされた時
+				if (touch.phase == TouchPhase.Began && tag == "LeftFripperTag") {
+					SetAngel (this.flickAngel);
+				}
+				//
+				if (touch.phase == TouchPhase.Ended && tag == "LeftFripperTag" || tag == "RightFripperTag") {
+					SetAngel (this.defaultAngel);
+				}
+					
+			}else if (touch.position.x > this.winBorder) {
+				//画面右側をタップされた時
+				if (touch.phase == TouchPhase.Began && tag == "RightFripperTag") {
+					SetAngel (this.flickAngel);
+				}
+				if (touch.phase == TouchPhase.Ended && tag == "RightFripperTag" || tag == "LeftFripperTag") {
+					SetAngel (this.defaultAngel);
+				}
+			}	
+		}
+	}
+
+	//フリッパーの傾きを設定
+	public void SetAngel(float angel){
+		JointSpring jointspr = this.myHingeJoint.spring;
+		jointspr.targetPosition = angel;
+		this.myHingeJoint.spring = jointspr;
+	}
+
+}
